8c162246e551c6a3a329e79ef9eefc6eb7245068
lph15925637437 <1409531757@qq.com>
2019-05-28 14:18:03 +0800

    '并行以及并发项目第一次提交'

---
+++ src/main/java/com/muti/thread/concurrent_competitor/ConcurrentCompetitorApplication.java
@@ -0,0 +1,13 @@
+package com.muti.thread.concurrent_competitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentCompetitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentCompetitorApplication.class, args);
+    }
+
+}
